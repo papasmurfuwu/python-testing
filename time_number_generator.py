682685e006c8a34621171f77d10efef16bc83470
papasmurfuwu <quentanwong@gmail.com>
2023-10-24 19:13:46 +0800

    changed some looping

--- time_number_generator.py
+++ time_number_generator.py
@@ -13,31 +13,37 @@ print('Welcome to the time/ number generator program!')
 def time_generator():
     again_time = True
     while again_time:
-        try:
-            user = int(input('Do you want time to be 12-hours or 24-hours (12/ 24)? '))
-            user_times = int(input('How many times do you want? '))
+        while True:
+            try:
+                user = int(input('Do you want time to be 12-hours or 24-hours (12/ 24)? '))
+                if user == 12 or user == 24:
+                    break
+                else:
+                    print('Invalid input. Please enter either 12 or 24.')
+                    continue
+            except ValueError:
+                print('Invalid input. Please enter a valid integer. ')
+                continue
 
-            if user == 24:
-                for i in range(user_times):
-                    hours = random.randint(0, 23)
-                    minutes = random.randint(0, 59)
-                    print(f"{hours:02}:{minutes:02}")
-                break
+        user_times = int(input('How many times do you want? '))
 
-            elif user == 12:
-                for i in range(user_times):
-                    hours = random.randint(1, 12)
-                    minutes = random.randint(0, 59)
-                    am_pm = random.choice(['AM', 'PM'])
-                    print(f"{hours:02}:{minutes:02} {am_pm}")
-                break
+        if user == 24:
+            for i in range(user_times):
+                hours = random.randint(0, 23)
+                minutes = random.randint(0, 59)
+                print(f"{hours:02}:{minutes:02}")
+            break
 
-            else:
-                raise ValueError('Please enter either 12 or 24!')
+        elif user == 12:
+            for i in range(user_times):
+                hours = random.randint(1, 12)
+                minutes = random.randint(0, 59)
+                am_pm = random.choice(['AM', 'PM'])
+                print(f"{hours:02}:{minutes:02} {am_pm}")
+            break
 
-        except ValueError as e:
-            print(e)
-            continue
+        else:
+            raise ValueError('Please enter either 12 or 24!')
 
 
 def num_generator():
@@ -73,20 +79,21 @@ if __name__ == '__main__':
             elif user_choice == 'number':
                 num_generator()
 
-            try:
-                again = input('Would you like to use the program again (y/n)? ').strip().lower()
-                if again == 'y':
-                    continue
-                elif again == 'n':
-                    print('Thanks for using us and have a great day!')
+            while True:
+                try:
+                    again = input('Would you like to use the program again (y/n)? ').strip().lower()
+                    if again == 'y':
+                        break
+
+                    elif again == 'n':
+                        print('Thanks for using us and have a great day!')
+                        again = False
+                        break
                     break
-            except ValueError:
-                print('Please enter y/n!')
+                except ValueError:
+                    print('Please enter y/n!')
+                    continue
 
         except ValueError:
             print('Please enter a valid string (time/ number)')
-
-
-
-
-
+            continue
